9b569384abeace9d04281c0aebd6374c7d76b84a
sahib2442 <kadersahib1234@gmail.com>
2022-08-26 16:28:14 +0530

    [sahib] Add: leap year

---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2012;
+            if (year % 4 == 0)
+            {
+                if ((year % 100 == 0) && (year % 400 != 0))
+                    Console.Write("{0} is not a leap year", year);
+                else
+                    Console.Write("{0} is a leap year", year);
+            }
+            else
+                Console.Write("{0} is not a leap year", year);
+        }
+    }
+}
+
---
+++ LeapYear/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LeapYear
+{
+    internal class program
+    {
+        static void main(string[]args)
+        {
+            Program program = new Program();
+            Console.WriteLine("leap year");
+        }
+    }
+}
